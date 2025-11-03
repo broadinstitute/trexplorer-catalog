4fa92e4a1ab2120d950aa979dd7c977cc9f94f0f
bw2 <ben.weisburd@gmail.com>
2025-11-02 19:21:04 -0500

    convert original vamos catalog to bed

--- scripts/convert_vamos_catalog_to_bed.py
+++ scripts/convert_vamos_catalog_to_bed.py
@@ -2,26 +2,35 @@ import os
 import pandas as pd
 from pprint import pprint
 
-if os.getcwd().endswith("str-truth-set-v2"):
-	os.chdir("str-truth-set/ref/other")
-elif os.getcwd().endswith("str-truth-set"):
-	os.chdir("ref/other")
-
 # source: https://github.com/ChaissonLab/vamos
 version = "v2.1"
-df = pd.read_table(
-	f"https://zenodo.org/records/11625069/files/vamos.motif.hg38.{version}.e0.1.tsv.gz?download=1",
-	compression="gzip",
-	names=["chrom", "start_1based", "end_1based", "motifs", "version", "STR_or_VNTR", "motif_size", "score1", "score2", "segdup", "is_coding"],
-)
+
+header = [
+	"chrom",
+	"start_1based",
+	"end_1based",
+	"motifs",
+	"version",
+	"STR_or_VNTR",
+	"motif_size",
+	"score1",
+	"score2",
+]
+
+#url = f"https://zenodo.org/records/11625069/files/vamos.motif.hg38.{version}.e0.1.tsv.gz?download=1"
+#header += ["segdup", "is_coding"]
+
+url = f"https://zenodo.org/records/11625069/files/vamos.motif.hg38.{version}.orig.tsv.gz?download=1"
+output_path_prefix = f"vamos_catalog.ori.{version}"
+
+df = pd.read_table(url, compression="gzip", names=header)
 df["start_0based"] = df["start_1based"] - 1
 df["locus_size"] = df["end_1based"] - df["start_0based"]
 df["motif1"] = df["motifs"].str.split(",").str[0]
-df["motif_count"] = df["motifs"].str.count(",") + 1
-df["is_coding"] = df["is_coding"] == "coding"
-df["segdup"] = df["segdup"] == "segDup"
+#df["motif_count"] = df["motifs"].str.count(",") + 1
+#df["is_coding"] = df["is_coding"] == "coding"
+#df["segdup"] = df["segdup"] == "segDup"
 
-output_path_prefix = f"vamos_catalog.{version}"
 df.to_csv(f"{output_path_prefix}.tsv.gz", sep="\t", index=False, header=True)
 
 df = df[["chrom", "start_0based", "end_1based", "motif1", "motif_size"]]
