da85f967070d9a4716c3445dfcca7c1b8bcc123b
bw2 <ben.weisburd@gmail.com>
2025-12-05 13:15:39 -0500

    compare with vamos

--- compare_with_loci_from_other_sources/loci_from_Danzi_2025/compare.sh
+++ compare_with_loci_from_other_sources/loci_from_Danzi_2025/compare.sh
@@ -1,16 +1,18 @@
 
-catalog=TRExplorer_v2:../../results__2025-11-22/1_to_1000bp_motifs/repeat_catalog_v2.hg38.1_to_1000bp_motifs.bed.gz
-
-echo ================================================================================================
-echo Comparing  to ${catalog}
-echo ================================================================================================
-set -ex
-python3 ../compare_loci_with_catalog.py --catalog ${catalog}  \
-  Danzi_2025_outliers__wide_boundaries:Danzi_2025_OE_or_LPSStdev_outliers.wide_boundaries.bed.gz \
-  Danzi_2025_outliers__narrow_boundaries_all:Danzi_2025_OE_or_LPSStdev_outliers.narrow_adotto_boundaries.all_overlapping_loci.bed.gz \
-  Danzi_2025_outliers__narrow_boundaries_only_matching_motif_lengths:Danzi_2025_OE_or_LPSStdev_outliers.narrow_adotto_boundaries.only_matching_motif_lengths.bed.gz \
-  Danzi_2025_outliers__narrow_boundaries_only_matching_motif_lengths_replaced_with_LPS_motif:Danzi_2025_OE_or_LPSStdev_outliers.narrow_adotto_boundaries.only_matching_motif_lengths_replaced_with_LPS_motif.bed.gz \
-  adotto_v1.2:adotto_tr_catalog_v1.2.bed.gz \
-  --print-stats 2 \
-  --write-bed-files-with-subsets
+for catalog in TRExplorer_v2:../../results__2025-11-22/1_to_1000bp_motifs/repeat_catalog_v2.hg38.1_to_1000bp_motifs.bed.gz \
+		   loci_from_vamos:../../results__2025-11-28/1_to_1000bp_motifs/repeat_catalog_v2.hg38.1_to_1000bp_motifs.merged2.vamos_catalog.ori.v2.1.filtered.unique_loci.bed.gz;
+do 
+    echo ================================================================================================
+    echo Comparing  to ${catalog}
+    echo ================================================================================================
+    set -ex
+    python3 ../compare_loci_with_catalog.py --catalog ${catalog}  \
+	    Danzi_2025_outliers__wide_boundaries:Danzi_2025_OE_or_LPSStdev_outliers.wide_boundaries.bed.gz \
+	    Danzi_2025_outliers__narrow_boundaries_all:Danzi_2025_OE_or_LPSStdev_outliers.narrow_adotto_boundaries.all_overlapping_loci.bed.gz \
+	    Danzi_2025_outliers__narrow_boundaries_only_matching_motif_lengths:Danzi_2025_OE_or_LPSStdev_outliers.narrow_adotto_boundaries.only_matching_motif_lengths.bed.gz \
+	    Danzi_2025_outliers__narrow_boundaries_only_matching_motifs:Danzi_2025_OE_or_LPSStdev_outliers.narrow_adotto_boundaries.only_matching_motifs.bed.gz \
+	    adotto_v1.2:adotto_tr_catalog_v1.2.bed.gz \
+	    --print-stats 2 \
+	    --write-bed-files-with-subsets
+done
 
