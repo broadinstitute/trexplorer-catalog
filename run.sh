f4f24f9011d942815db7f4237bc87704f36fbd8e
bw2 <ben.weisburd@gmail.com>
2026-02-17 17:45:39 -0500

    .

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ rm nohup.out
 
 set -ex
 
-nohup python3 -u run_all_steps_to_generate_the_catalog.py --timestamp 2026-01-12
+nohup python3 -u run_all_steps_to_generate_the_catalog.py --timestamp 2026-02-01  # --timestamp 2026-01-12
 
